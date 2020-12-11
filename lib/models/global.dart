f77caeb7af75e64cc4f22178e6aa95a364bd65ae
Dylan Fuhri <dylan.fuhri@gmail.com>
2020-12-11 13:38:26 +0200

    initial commit

---
+++ lib/models/global.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color globalGrey = new Color(0xffc6c6c6);
+Color globalGreen = new Color(0xff47747b);
+Color globalLightGrey = new Color(0xfff2f2f2);
+Color globalLightGreen = new Color(0xff90be53);
